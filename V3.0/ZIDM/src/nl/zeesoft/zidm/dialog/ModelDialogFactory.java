fe98ba29c9882e0fe0048a8fd1bf1932ad8a8fc2
André van der Zee <dyz@xs4all.nl>
2017-02-05 22:47:23 +0100

    Started english dialog

--- V3.0/ZIDM/src/nl/zeesoft/zidm/dialog/ModelDialogFactory.java
+++ V3.0/ZIDM/src/nl/zeesoft/zidm/dialog/ModelDialogFactory.java
@@ -1,5 +1,66 @@
 package nl.zeesoft.zidm.dialog;
 
-public class ModelDialogFactory {
+import nl.zeesoft.zid.dialog.Dialog;
+import nl.zeesoft.zspr.Language;
 
+public class ModelDialogFactory {
+	public static final String Modelling				= "Modelling";
+	
+	public static final String classNameSingle			= "classNameSingle";
+	public static final String classNameMulti			= "classNameMulti";
+	public static final String extendedClassNameSingle	= "extendedClassNameSingle";
+	public static final String extendedClassNameMulti	= "extendedClassNameMulti";
+	public static final String propertyNameSingle		= "propertyNameSingle";
+	public static final String propertyNameMulti		= "propertyNameMulti";
+	
+	public static final String defaultAnswer			= "Interesting.";
+	
+	public Dialog getEnglishModelDialog() {
+		Dialog dialog = new Dialog(Modelling,Language.ENG,ModelDialogController.class.getName());
+		dialog.addExample("A {" + classNameSingle + "} is a {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("A {" + classNameSingle + "} is an {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("An {" + classNameSingle + "} is a {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("An {" + classNameSingle + "} is an {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("A {" + classNameSingle + "} is a kind of {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("An {" + classNameSingle + "} is a kind of {" + extendedClassNameSingle + "}.",defaultAnswer);
+		dialog.addExample("{" + classNameMulti + "} are {" + extendedClassNameMulti + "}.",defaultAnswer);
+		dialog.addExample("{" + classNameMulti + "} are a kind of {" + extendedClassNameMulti + "}.",defaultAnswer);
+
+		for (int i = 0; i < 6; i++) {
+			String propertyAttachmentS = " with {" + propertyNameMulti + "}";
+			String propertyAttachmentM = " with {" + propertyNameMulti + "}";
+			if (i==1) {
+				propertyAttachmentS = " with multiple {" + propertyNameMulti + "}";
+				propertyAttachmentM = " with multiple {" + propertyNameMulti + "}";
+			} else if (i==2) {
+				propertyAttachmentS = " with one or more {" + propertyNameMulti + "}";
+				propertyAttachmentM = " with one or more {" + propertyNameMulti + "}";
+			} else if (i==3) {
+				propertyAttachmentS = " that has {" + propertyNameMulti + "}";
+				propertyAttachmentM = " that have {" + propertyNameMulti + "}";
+			} else if (i==4) {
+				propertyAttachmentS = " that has a {" + propertyNameSingle + "}";
+				propertyAttachmentM = " that have a {" + propertyNameSingle + "}";
+			} else if (i==5) {
+				propertyAttachmentS = " that has an {" + propertyNameSingle + "}";
+				propertyAttachmentM = " that have an {" + propertyNameSingle + "}";
+			} else if (i==4) {
+				propertyAttachmentS = " with a {" + propertyNameSingle + "}";
+				propertyAttachmentM = " with a {" + propertyNameSingle + "}";
+			} else if (i==5) {
+				propertyAttachmentS = " with an {" + propertyNameSingle + "}";
+				propertyAttachmentM = " with an {" + propertyNameSingle + "}";
+			}
+			dialog.addExample("A {" + classNameSingle + "} is a {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("A {" + classNameSingle + "} is an {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("An {" + classNameSingle + "} is a {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("An {" + classNameSingle + "} is an {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("A {" + classNameSingle + "} is a kind of {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("An {" + classNameSingle + "} is a kind of {" + extendedClassNameSingle + "}" + propertyAttachmentS + ".",defaultAnswer);
+			dialog.addExample("{" + classNameMulti + "} are {" + extendedClassNameMulti + "}" + propertyAttachmentM + ".",defaultAnswer);
+			dialog.addExample("{" + classNameMulti + "} are a kind of {" + extendedClassNameSingle + "}" + propertyAttachmentM + ".",defaultAnswer);
+		}
+		
+		return dialog;
+	}
 }
