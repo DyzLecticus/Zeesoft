8170180b7a24cbf5fe0c3b1b5fa9b9754243b965
Andre van der Zee <andre.van.der.zee@nl.eu.abnamro.com>
2017-06-09 17:26:36 +0200

    Refactored manual

--- V3.0/ZMMT/src/nl/zeesoft/zmmt/ZeeTracker.java
+++ V3.0/ZMMT/src/nl/zeesoft/zmmt/ZeeTracker.java
@@ -18,12 +18,12 @@ public class ZeeTracker {
 		r.append("==========\n");
 		r.append("ZeeTracker is designed to provide a fast music creating workflow.\n");
 		r.append("The interface is based on mod trackers like Scream Tracker and Impulse Tracker.\n");
-		r.append("This means that the program is made for keyboard control.\n");
-		r.append("For instance, if you explore the menu bar you will see that most menu options have keyboard shortcuts.\n");
+		r.append("This means that ZeeTracker is made for keyboard control.\n");
+		r.append("Mouse control is supported but not needed to create music with ZeeTracker.\n");
 		r.append("\n");
 		r.append("Menu options\n");
 		r.append("------------\n");
-		r.append("File\n");
+		r.append("**File**  \n");
 		r.append(" * Load (Ctrl+L)  \n");
 		r.append("   Presents a file dialog allowing the user to load a ZeeTracker composition file (*.ztc).\n");
 		r.append(" * Save (Ctrl+S)  \n");
@@ -35,6 +35,48 @@ public class ZeeTracker {
 		r.append("   Creates a new ZeeTracker composition file based on the current settings.\n");
 		r.append(" * Demo  \n");
 		r.append("   Loads the ZeeTracker demo composition.\n");
+		r.append("**Show**  \n");
+		r.append(" * Composition (F1)  \n");
+		r.append("   Switches to the composition tab.\n");
+		r.append(" * Instruments (F2)  \n");
+		r.append("   Switches to the instruments tab.\n");
+		r.append(" * Patterns (F3)  \n");
+		r.append("   Switches to the patterns tab.\n");
+		r.append(" * Sequence (F4)  \n");
+		r.append("   Switches to the pattern sequence tab.\n");
+		r.append(" * Settings (F9)  \n");
+		r.append("   Switches to the settings tab.\n");
+		// TODO: Include manual
+		r.append("**Instrument**  \n");
+		r.append("This menu provides options to select a specific working instrument.\n");
+		r.append("The currently selected instrument is reflected in the background color of the tab selectors.\n");
+		r.append(" * Synth bass 1 (Ctrl+1)  \n");
+		r.append(" * Synth bass 2 (Ctrl+2)  \n");
+		r.append(" * Synth bass 3 (Ctrl+3)  \n");
+		r.append(" * Synth 1 (Ctrl+4)  \n");
+		r.append(" * Synth 2 (Ctrl+5)  \n");
+		r.append(" * Synth 3 (Ctrl+6)  \n");
+		r.append(" * Lead (Ctrl+7)  \n");
+		// TODO: Switch drums and strings
+		r.append(" * Strings (Ctrl+8)  \n");
+		r.append(" * Drums (Ctrl+9)  \n");
+		r.append(" * Echo (Ctrl+0)  \n");
+		r.append("Edit\n");
+		r.append(" * Undo (Ctrl+Z)  \n");
+		r.append("   Undoes the latest composition change.\n");
+		r.append(" * Redo (Ctrl+Y)  \n");
+		r.append(" * Pattern  \n");
+		r.append("   * Select pattern (Ctrl+P)  \n");
+		r.append("   * Toggle insert (Ctrl+I)  \n");
+		r.append("   * Edit notes (Ctrl+E)  \n");
+		r.append("   * Copy notes (Ctrl+C)  \n");
+		r.append("   * Paste notes (Ctrl+V)  \n");
+		r.append("Sequencer\n");
+		r.append(" * Play pattern (F5)  \n");
+		r.append(" * Play sequence (F6)  \n");
+		r.append(" * Continue (F7)  \n");
+		r.append(" * Pause/Stop (F8)  \n");
+		// TODO: Finish manual
 		return r;
 	}
 }
