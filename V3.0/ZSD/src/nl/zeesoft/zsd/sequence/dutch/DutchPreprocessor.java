76ef36a715e3ea72cab93557188fba7da92eec2e
André van der Zee <dyz@xs4all.nl>
2018-07-16 00:37:16 +0200

    Simplified

--- V3.0/ZSD/src/nl/zeesoft/zsd/sequence/dutch/DutchPreprocessor.java
+++ V3.0/ZSD/src/nl/zeesoft/zsd/sequence/dutch/DutchPreprocessor.java
@@ -5,12 +5,10 @@ import nl.zeesoft.zsd.sequence.SequencePreprocessor;
 public class DutchPreprocessor extends SequencePreprocessor {
 	@Override
 	public void initialize() {
-		addReplacement("smorgensvroeg","smorgens vroeg");
-		addReplacement("savondslaat","savonds laat");
-		addReplacement("morgensvroeg","smorgens vroeg");
-		addReplacement("avondslaat","savonds laat");
 		addReplacement("'s ochtends","sochteds");
 		addReplacement("'s middags","smiddags");
 		addReplacement("'s avonds","savonds");
+		addReplacement("morgensvroeg","morgens vroeg");
+		addReplacement("avondslaat","avonds laat");
 	}
 }
