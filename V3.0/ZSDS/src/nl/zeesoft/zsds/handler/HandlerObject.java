a45de8f6db60871294df4781345a4633f9461d43
André van der Zee <dyz@xs4all.nl>
2018-07-23 22:09:05 +0200

    Added handler and index html resource

---
+++ V3.0/ZSDS/src/nl/zeesoft/zsds/handler/HandlerObject.java
@@ -0,0 +1,43 @@
+package nl.zeesoft.zsds.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import nl.zeesoft.zdk.ZStringBuilder;
+import nl.zeesoft.zsds.AppConfiguration;
+
+public abstract class HandlerObject {
+	private	AppConfiguration	configuration	= null;
+	private String				path			= "";
+	private ZStringBuilder		cachedResponse	= null;
+	
+	public HandlerObject(AppConfiguration config,String path) {
+		this.configuration = config;
+		this.path = path;
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		// Override to implement
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		// Override to implement
+	}
+	
+	protected abstract ZStringBuilder buildResponse();
+	
+	protected ZStringBuilder getCachedResponse() {
+		if (cachedResponse==null) {
+			cachedResponse = buildResponse();
+		}
+		return cachedResponse;
+	}
+
+	protected AppConfiguration getConfiguration() {
+		return configuration;
+	}
+
+	public String getPath() {
+		return path;
+	}
+}
