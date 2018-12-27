0b66017fadc271e1d61b62127437d956e9c43c57
André van der Zee <dyz@xs4all.nl>
2018-12-27 10:45:30 +0100

    Different approach

--- V4.0/ZSC/src/nl/zeesoft/zsc/confab/SymbolClusterer.java
+++ V4.0/ZSC/src/nl/zeesoft/zsc/confab/SymbolClusterer.java
@@ -22,6 +22,47 @@ public class SymbolClusterer extends Confabulator {
 		
 		Context ctxt = getContextNoLock(contextSymbol);
 		if (ctxt.totalLinks>0) {
+			SortedMap<String,List<Link>> linksPerSymbol = new TreeMap<String,List<Link>>();
+			SortedMap<Integer,List<String>> symbolsByCount = new TreeMap<Integer,List<String>>();
+			for (String symbol: ctxt.knownSymbols) {
+				if (!ZStringSymbolParser.isLineEndSymbol(symbol) &&
+					!ZStringSymbolParser.isPunctuationSymbol(symbol)
+					) {
+					List<Link> links = new ArrayList<Link>();
+					int count = 0;
+					for (int i = 1; i<= getMaxDistance(); i++) {
+						List<Link> linksFrom = this.getLinksNoLock(symbol,i,contextSymbol,"",caseSensitive);
+						for (Link lnk: linksFrom) {
+							if (lnk.count>1 && 
+								!ZStringSymbolParser.isLineEndSymbol(lnk.symbolTo) &&
+								!ZStringSymbolParser.isPunctuationSymbol(lnk.symbolTo)
+								) {
+								links.add(lnk);
+								count += lnk.count;
+							}
+						}
+						List<Link> linksTo = this.getLinksNoLock("",i,contextSymbol,symbol,caseSensitive);
+						for (Link lnk: linksTo) {
+							if (lnk.count>1 && 
+								!ZStringSymbolParser.isLineEndSymbol(lnk.symbolFrom) &&
+								!ZStringSymbolParser.isPunctuationSymbol(lnk.symbolFrom)
+								) {
+								links.add(lnk);
+								count += lnk.count;
+							}
+						}
+					}
+					if (links.size()>0) {
+						linksPerSymbol.put(symbol,links);
+						symbolsByCount.put(count,symbol);
+					}
+				}
+			}
+			for (Entry<Integer,String> entry: symbolsByCount.entrySet()) {
+				System.out.println(entry.getValue() + ": " + entry.getKey());
+			}
+			
+			/*
 			List<Link> links = getContextNoLock(contextSymbol).getAllLinks(caseSensitive);
 			
 			SortedMap<Integer,List<Link>> linksByCount = new TreeMap<Integer,List<Link>>();
@@ -53,6 +94,7 @@ public class SymbolClusterer extends Confabulator {
 					}
 				}
 			}
+			*/
 		}
 		
 		unlockMe(this);
