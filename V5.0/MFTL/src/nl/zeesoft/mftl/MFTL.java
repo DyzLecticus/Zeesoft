6848a7f164432750fd5025bec0e7db82185d45bd
André van der Zee <dyz@xs4all.nl>
2020-07-21 02:50:37 +0200

    Added backup save game before start

--- V5.0/MFTL/src/nl/zeesoft/mftl/MFTL.java
+++ V5.0/MFTL/src/nl/zeesoft/mftl/MFTL.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Calendar;
 
 import nl.zeesoft.zdk.Str;
 
@@ -11,7 +16,10 @@ public class MFTL  {
 	private static final String		EXECUTABLE			= "MinecraftLauncher.exe";
 	private static final String		USERCACHE			= "/AppData/Roaming/.minecraft/usercache.json";
 	private static final String		FIND_PREFIX			= "\"expiresOn\":\"20";
-	
+
+	private static final String		SAVES				= "/AppData/Roaming/.minecraft/saves/";
+	private static final String		DEMO_WORLD			= "Demo_World/";
+
 	private static String 			launcherInstallPath	= "C:\\Program Files (x86)\\Minecraft Launcher";
 	
 	public static void main(String[] args) {
@@ -22,15 +30,16 @@ public class MFTL  {
 			if (err.length()>0) {
 				System.err.println(err);
 			} else {
+				backupDemoWorld();
 				runLauncher(l);
 			}
 		}
 	}
 	
 	private static String getUserCacheFilePath() {
-		String dir = System.getProperty("user.home");
-		dir += USERCACHE;
-		return dir;
+		String path = System.getProperty("user.home");
+		path += USERCACHE;
+		return path;
 	}
 	
 	private static boolean checkFileExists(File f) {
@@ -50,6 +59,34 @@ public class MFTL  {
 		}
 		System.out.println(u.getAbsolutePath() + ": " + userCache);
 		return userCache.toFile(u.getAbsolutePath());
+	}
+	
+	private static void backupDemoWorld() {
+		File s = new File(getSavesPath() + DEMO_WORLD);
+		if (s.exists()) {
+			Calendar cal = Calendar.getInstance();
+			String timeStamp = cal.get(Calendar.YEAR)
+			    + "-" + String.format("%02d", (cal.get(Calendar.MONTH) + 1))
+			    + "-" + String.format("%02d", cal.get(Calendar.DAY_OF_MONTH))
+			    + "-"+ String.format("%02d", cal.get(Calendar.HOUR_OF_DAY))
+				;
+			String path = getSavesPath() + timeStamp + "-" + DEMO_WORLD;
+			File t = new File(path);
+			System.out.println("Creating backup: " + t.getAbsolutePath());
+	        Path source = Paths.get(s.getAbsolutePath());
+	        Path target = Paths.get(t.getAbsolutePath());
+	        try {
+				Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static String getSavesPath() {
+		String path = System.getProperty("user.home");
+		path += SAVES;
+		return path;
 	}
 	
 	private static void runLauncher(File l) {
