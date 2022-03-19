272d118f531d6fd73fc5fe36f5dff97d9e273b9b
André van der Zee <dyz@xs4all.nl>
2022-03-19 19:13:20 +0100

    Added depth constructor argument support

--- NodeJS/predictive-memory/src/predict/PredictorConfig.js
+++ NodeJS/predictive-memory/src/predict/PredictorConfig.js
@@ -3,10 +3,10 @@ const IndexGenerator = require('./IndexGenerator');
 const CacheConfig = require('../cache/CacheConfig');
 const Transformer = require('../Transformer');
 
-function PredictorConfig(max) {
-  this.maxHistorySize = max || 1000;
+function PredictorConfig(size, depth) {
+  this.maxHistorySize = size || 1000;
   this.comparator = new Comparator();
-  this.cacheIndexes = (new IndexGenerator()).generate();
+  this.cacheIndexes = (new IndexGenerator(depth)).generate();
   this.cacheConfig = new CacheConfig();
   this.transformer = new Transformer();
 }
--- NodeJS/predictive-memory/test/unit/predict/PredictorConfig.test.js
+++ NodeJS/predictive-memory/test/unit/predict/PredictorConfig.test.js
@@ -6,7 +6,8 @@ describe('PredictorConfig', () => {
     expect(pc.maxHistorySize).toBe(1000);
     expect(pc.cacheConfig).not.toBe(null);
     expect(pc.cacheIndexes.length).toBe(15);
-    pc = new PredictorConfig(3);
+    pc = new PredictorConfig(3, 6);
     expect(pc.maxHistorySize).toBe(3);
+    expect(pc.cacheIndexes.length).toBe(10);
   });
 });
